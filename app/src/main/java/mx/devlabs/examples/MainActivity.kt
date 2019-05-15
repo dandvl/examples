64b4be05f763c6feeef32a9f733792d3e3c2190f
Raúl Maese <rmaese@blacklabs.mx>
2019-05-15 19:21:00 -0400

    Se agrego ejemplo para manejo de fragmentos.

--- app/src/main/java/mx/devlabs/examples/MainActivity.kt
+++ app/src/main/java/mx/devlabs/examples/MainActivity.kt
@@ -1,9 +1,16 @@
 package mx.devlabs.examples
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+        FragmentOne.OnFragmentInteractionListener,
+        FragmentTwo.OnFragmentInteractionListener {
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
