3ffd87fbe7e619de7d0ebc5b6f80ebd940495fea
Raúl Maese <rmaese@blacklabs.mx>
2019-06-13 19:13:01 -0400

    Broadcast receiver for airplane mode.

---
+++ app/src/main/java/mx/devlabs/examples/MyReceiver.kt
@@ -0,0 +1,14 @@
+package mx.devlabs.examples
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class MyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        Log.i("RMC", "modo avion!!")
+    }
+}
